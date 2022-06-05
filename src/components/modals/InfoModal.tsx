c56558237f7a32f15e81e3b53e5c66feef9b88c6
WarriorAFII <dj_afnan@hotmail.com>
2022-06-05 11:15:41 +0100

    changes

---
+++ src/components/modals/InfoModal.tsx
@@ -0,0 +1,62 @@
+import { Cell } from '../grid/Cell'
+import { BaseModal } from './BaseModal'
+
+type Props = {
+  isOpen: boolean
+  handleClose: () => void
+}
+
+export const InfoModal = ({ isOpen, handleClose }: Props) => {
+  return (
+    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Guess the word in 6 tries. After each guess, the color of the tiles will
+        change to show how close your guess was to the word.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="M" status="correct" />
+        <Cell value="O" />
+        <Cell value="U" />
+        <Cell value="N" />
+        <Cell value="T" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter W is in the word and in the correct spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="M" />
+        <Cell value="E" />
+        <Cell value="N" status="present" />
+        <Cell value="D" />
+        <Cell value="Y" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter L is in the word but in the wrong spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="S" />
+        <Cell value="A" />
+        <Cell value="L" />
+        <Cell value="A" status="absent" />
+        <Cell value="H" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter U is not in the word in any spot.
+      </p>
+
+      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+        This is an open source version of the word guessing game we all know and
+        love -{' '}
+        <a
+          href="https://github.com/cwackerfuss/react-wordle"
+          className="underline font-bold"
+        >
+          check out the code here
+        </a>{' '}
+      </p>
+    </BaseModal>
+  )
+}
